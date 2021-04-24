60d5826aa0754b65a0d8c8012989610547a0b389
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-04-24 19:40:18 +0000

    Auth

--- App.tsx
+++ App.tsx
@@ -3,23 +3,25 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddAssistance from './app/assistances/screens/AddAssistance';
 import Constants from 'expo-constants'
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import ListNavigation from './app/shared/components/ListNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {SignIn} from './app/auth/sign-in/sign-in';
 import {SignUp} from './app/auth/sign-up/sign-up';
+import AddBreakfast from './app/breakfasts/screens/AddBreakfast';
 
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAP2O_RSdni3pwPbK_cgKuv7UIChwSrXcE",
-  authDomain: "errahmanetable.firebaseapp.com",
-  projectId: "errahmanetable",
-  storageBucket: "errahmanetable.appspot.com",
-  messagingSenderId: "223055466573",
-  appId: "1:223055466573:web:50cbe6868b9d1a38860c04",
-  measurementId: "G-7LQL2MVEBW"
-};
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAP2O_RSdni3pwPbK_cgKuv7UIChwSrXcE",
+//   authDomain: "errahmanetable.firebaseapp.com",
+//   projectId: "errahmanetable",
+//   storageBucket: "errahmanetable.appspot.com",
+//   messagingSenderId: "223055466573",
+//   appId: "1:223055466573:web:50cbe6868b9d1a38860c04",
+//   measurementId: "G-7LQL2MVEBW"
+// };
+
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
 
@@ -59,8 +61,16 @@ export default function App() {
             backgroundColor: 'skyblue',
           }, 
         }}/> 
+        
         <Stack.Screen name="Assistance" component={AddAssistance} options={{
           title: "Add Assistance",
+          headerTintColor:'white',
+          headerStyle:{
+            backgroundColor: 'skyblue',
+          }, 
+        }} /> 
+        <Stack.Screen name="Assist" component={AddBreakfast} options={{
+          title: "Add Brak fast",
           headerTintColor:'white',
           headerStyle:{
             backgroundColor: 'skyblue',
--- app/ramadan/services/ramadan-service.tsx
+++ app/ramadan/services/ramadan-service.tsx
@@ -1,18 +1,18 @@
-import admin from 'firebase-admin';
+// // import admin from 'firebase-admin';
 
-import Timestamp = admin.firestore.Timestamp;
+// import Timestamp = admin.firestore.Timestamp;
 
-export class RamadanService {
-    constructor(){ }
+// export class RamadanService {
+//     constructor(){ }
 
-    add(ramadan : any) {
-        console.log(ramadan);
-        console.log("ajouté le :"+Timestamp);
-        /*
-        *
-        *
-        * Ici c'est le code d'insertion dans la base de données Firestore
-        * 
-        */ 
-      }
-}
+//     add(ramadan : any) {
+//         console.log(ramadan);
+//         console.log("ajouté le :"+Timestamp);
+//         /*
+//         *
+//         *
+//         * Ici c'est le code d'insertion dans la base de données Firestore
+//         * 
+//         */ 
+//       }
+// }
